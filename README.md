d153dc8a9ea5f59a43e4972f712ddd998449f3a2
Digital-Thought <matthew@digital-thought.org>
2021-08-06 15:24:12 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Digital-Thought, Digital Forensics & Incident Response Library
+## Version: v0.1.0
+
+
+### Installation Instructions
+
+To install the most recent release from PyPI:<br>
+```shell script
+pip install digital-thought-dfir
+```
+***
+## Features
+
+***
+## Licence
+This work is made freely available under the Version 3, GNU AFFERO GENERAL PUBLIC LICENSE.
+
+***
+## Contact, Questions and Issues
+If you have any questions, please email [development@digital-thought.org](mailto:development@digital-thought.org?subject=[Question:digital-thought-dfir])<br>
+If you encounter any issues in the use of this library, please log them via https://github.com/Digital-Thought/commons/dfir" <br>
+If you would like to contribute to this library, please email [development@digital-thought.org](mailto:development@digital-thought.org?subject=[Question:digital-thought-dfir])
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+with open("digital_thought_dfir/version", "r") as fh:
+    version_info = fh.read()
+
+setuptools.setup(
+    name="digital_thought_dfir",
+    version=version_info,
+    author="Digital Thought",
+    author_email="development@digital-thought.org",
+    description="Digital Forensics & Incident Response Python Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Digital-Thought/dfir",
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    packages=find_packages(exclude=('unit_tests', 'tests', 'docs', 'sampleConfigs')),
+    python_requires='>=3.8',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'ip_enrichment = digital_thought_dfir.enrichers:main',
+        ],
+    }
+)
